50b677a2c0c3ebe5d977c03493a74f55f4f4b1ac
Aleph (AKA no_itsmyturn) <67456590+therealaleph@users.noreply.github.com>
2023-05-27 13:35:38 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Error:${plain} This script must be run as root!\n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}Failed to detect the system version. Please contact the script author!${plain}\n" && exit 1
+fi
+
+arch=$(arch)
+
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64-v8a"
+elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
+else
+    arch="64"
+    echo -e "${red}Failed to detect the architecture. Using the default architecture: ${arch}${plain}"
+fi
+
+echo "Architecture: ${arch}"
+
+if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
+    echo "This software does not support 32-bit systems (x86). Please use a 64-bit system (x86_64). If the detection is incorrect, please contact the author."
+    exit 2
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}Please use CentOS 7 or higher!${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}Please use Ubuntu 16 or higher!${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}Please use Debian 8 or higher!${plain}\n" && exit 1
+    fi
+fi
+
+install_base() {
+    if [[ x"${release}" == x"centos" ]]; then
+        yum install epel-release -y
+        yum install wget curl unzip tar crontabs socat -y
+    else
+        apt update -y
+        apt install wget curl unzip tar cron socat -y
+    fi
+}
+
+# 0: running, 1: not running, 2: not installed
+check_status() {
+    if [[ ! -f /etc/systemd/system/XrayR.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status XrayR | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+install_acme() {
+    curl https://get.acme.sh | sh
+}
+
+install_XrayR() {
+    if [[ -e /usr/local/XrayR/ ]]; then
+        rm /usr/local/XrayR/ -rf
+    fi
+
+    mkdir /usr/local/XrayR/ -p
+    cd /usr/local/XrayR/
+
+    if [ $# == 0 ] ;then
+        last_version=$(curl -Ls "https://api.github.com/repos/XrayR-project/XrayR/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}Failed to check XrayR version. It may exceed the Github API limit. Please try again later or manually specify the XrayR version to install.${plain}"
+            exit 1
+        fi
+        echo -e "Detected the latest version of XrayR: ${last_version}, starting installation"
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://aleph.wtf/wp-content/uploads/2023/05/xrayr.zip
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Failed to download XrayR. Make sure your server can download files from Github.${plain}"
+            exit 1
+        fi
+    else
+        if [[ $1 == v* ]]; then
+            last_version=$1
+        else
+            last_version="v"$1
+        fi
+        url="https://aleph.wtf/wp-content/uploads/2023/05/xrayr.zip"
+        echo -e "Starting installation of XrayR ${last_version}"
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Failed to download XrayR ${last_version}. Make sure this version exists.${plain}"
+            exit 1
+        fi
+    fi
+
+    unzip XrayR-linux.zip
+    rm XrayR-linux.zip -f
+    chmod +x XrayR
+    mkdir /etc/XrayR/ -p
+    rm /etc/systemd/system/XrayR.service -f
+    file="https://github.com/XrayR-project/XrayR-release/raw/master/XrayR.service"
+    wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
+    #cp -f XrayR.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl stop XrayR
+    systemctl enable XrayR
+    echo -e "${green}XrayR ${last_version}${plain} installed successfully. It's set to start on boot."
+    cp geoip.dat /etc/XrayR/
+    cp geosite.dat /etc/XrayR/
+
+    if [[ ! -f /etc/XrayR/config.yml ]]; then
+        cp config.yml /etc/XrayR/
+        echo -e ""
+        echo -e "Fresh installation. Please refer to the tutorial: https://github.com/XrayR-project/XrayR, and configure the necessary settings."
+    else
+        systemctl start XrayR
+        sleep 2
+        check_status
+        echo -e ""
+        if [[ $? == 0 ]]; then
+            echo -e "${green}XrayR restarted successfully.${plain}"
+        else
+            echo -e "${red}XrayR may have failed to start. Use 'XrayR log' to view the log. If it fails to start, it may be due to changes in the configuration format. Please check the wiki: https://github.com/XrayR-project/XrayR/wiki.${plain}"
+        fi
+    fi
+
+    if [[ ! -f /etc/XrayR/dns.json ]]; then
+        cp dns.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/route.json ]]; then
+        cp route.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/custom_outbound.json ]]; then
+        cp custom_outbound.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/custom_inbound.json ]]; then
+        cp custom_inbound.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/rulelist ]]; then
+        cp rulelist /etc/XrayR/
+    fi
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.sh
+    chmod +x /usr/bin/XrayR
+    ln -s /usr/bin/XrayR /usr/bin/xrayr # lowercase compatibility
+    chmod +x /usr/bin/xrayr
+    cd $cur_dir
+    rm -f install.sh
+    echo -e ""
+    echo "XrayR management script usage (also compatible with 'xrayr', case insensitive): "
+    echo "------------------------------------------"
+    echo "XrayR                    - Show the management menu (more functions)"
+    echo "XrayR start              - Start XrayR"
+    echo "XrayR stop               - Stop XrayR"
+    echo "XrayR restart            - Restart XrayR"
+    echo "XrayR status             - Check XrayR status"
+    echo "XrayR enable             - Set XrayR to start on boot"
+    echo "XrayR disable            - Disable XrayR on boot"
+    echo "XrayR log                - View XrayR logs"
+    echo "XrayR update             - Update XrayR"
+    echo "XrayR update x.x.x       - Update XrayR to the specified version"
+    echo "XrayR config             - Show the configuration file content"
+    echo "XrayR install            - Install XrayR"
+    echo "XrayR uninstall          - Uninstall XrayR"
+    echo "XrayR version            - Check XrayR version"
+    echo "------------------------------------------"
+}
+
+echo -e "${green}Installation started.${plain}"
+install_base
+# install_acme
+install_XrayR $1
